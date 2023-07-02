fa2bbae65cf76eeecb827e8227ce64b1432e52f3
StasPromist <ctacbars@gmail.com>
2023-07-02 22:22:34 +0300

    Added new Project Photo Editor

---
+++ src/SidebarItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SidebarItem({ name, active, handleClick }) {
+    return (
+        <button
+            className={`sidebar-item ${active ? 'active' : ''}`}
+            onClick={handleClick}
+        >
+
+            {name}
+        </button>
+    );
+}
+
+export default SidebarItem;
